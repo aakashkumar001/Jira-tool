2c39e7da883c7655d0eb5527b6d87f7d4efdc82a
aakashkr <aspakash000@gmail.com>
2024-10-16 08:50:07 +0530

    dashboard-layout-done

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,15 +1,16 @@
 import { getCurrent } from "@/features/auth/actions";
-import { UserButton } from "@/features/auth/components/user-button";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-   const user = await getCurrent()
+   const user = await getCurrent();
 
-   if(!user) redirect("/sign-in")
+   if(!user) {
+    redirect("/sign-in")
+   } 
 
   return (
     <div className="flex gap-4">
-      <UserButton />
+       This is a Home Page
     </div>
   );
 }
--- features/auth/components/user-button.tsx
+++ features/auth/components/user-button.tsx
@@ -38,7 +38,7 @@ export const UserButton = () => {
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger className="outline-none relative">
         <Avatar className="size-10 hover:opacity-75 transition-border font-medium border-neutral-300">
-          <AvatarFallback className="bg-neutal-200 font-medium text-neutral-500 flex items-center justify-center ">
+          <AvatarFallback className="bg-neutral-200 font-medium text-neutral-500 flex items-center justify-center ">
             {avatarFallback}
           </AvatarFallback>
         </Avatar>
@@ -46,7 +46,7 @@ export const UserButton = () => {
       <DropdownMenuContent align="end" side="bottom" className="w-60" sideOffset={10}>
         <div className="flex flex-col items-center justify-center gap-2 px-2.5 py-4">
           <Avatar className="size-[52px] border border-neutral-300">
-            <AvatarFallback className="bg-neutarl-200 text-xl font-medium text-neutral-500">
+            <AvatarFallback className="bg-neutral-200 text-xl font-medium text-neutral-500">
               {avatarFallback}
             </AvatarFallback>
           </Avatar>
